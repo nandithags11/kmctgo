893756864b22908171a438a8633e2b89836b39bd
nandithags11 <59022568+nandithags11@users.noreply.github.com>
2020-12-18 12:52:34 +0530

    Add files via upload

---
+++ day1.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		id   int     = 02
+		name string  = "nanditha"
+		mark float64 = 47.11
+	)
+	fmt.Printf("my id is %d and my name is %s also my mark is %0.2f.\n", id, name, mark)
+}
